2dc3b8f4eb50e4d527e7c48e8897bc55af65fbb8
Suskcx01 <sushmakc2052@gmail.com>
2024-04-29 21:17:07 +0200

    bug add

---
+++ Car.py
@@ -0,0 +1,20 @@
+class Car:
+    def __init__(self, model, name, color):
+        self.model = model
+        self.name = name
+        self.color = color
+
+    def get_model(self):
+        return self.model
+
+    def get_name(self):
+        return self.name
+
+    def get_color(self):
+        return self.color
+
+# Example usage
+car Car("2022", "Toyota", "Red")
+print("Model:", car.get_model())  # Output: Model: 2022
+print("Name:", car.get_name())    # Output: Name: Toyota
+print("Color:", car.get_color())  # Output: Color: Red
